7be993823e432370ed248af5536c3714de0d8da1
Vladimir Zasorin <zasorinvlad88@gmail.com>
2026-07-09 10:28:03 +0300

    Create new contacts

--- pages/modules/contacts/contacts.js
+++ pages/modules/contacts/contacts.js
@@ -1,88 +1,3 @@
-async function loadContacts(){
-
-
-const {data,error}=await db
-.from("contacts")
-.select("*")
-.order("created_at",{ascending:false});
-
-
-if(error){
-
-console.error(error);
-return;
-
-}
-
-
-let rows="";
-
-
-data.forEach(c=>{
-
-
-rows += `
-
-<tr>
-
-<td>${c.name}</td>
-
-<td>${c.type ?? ""}</td>
-
-<td>${c.platform ?? ""}</td>
-
-<td>${c.email ?? ""}</td>
-
-<td>${c.country ?? ""}</td>
-
-<td>${c.status}</td>
-
-<td>
-
-<button onclick="deleteContact(${c.id})">
-🗑
-</button>
-
-</td>
-
-</tr>
-
-`;
-
-
-});
-
-
-document.querySelector(
-"#contactsTable tbody"
-).innerHTML=rows;
-
-
-}
-
-
-
-async function deleteContact(id){
-
-
-if(!confirm("Удалить контакт?"))
-return;
-
-
-await db
-.from("contacts")
-.delete()
-.eq("id",id);
-
-
-loadContacts();
-
-
-}
-
-
-window.loadContacts=loadContacts;
-
 async function addContact(){
 
     const name=$("#cName").val();
@@ -124,3 +39,16 @@ async function addContact(){
     loadContacts();
 
 }
+$("#btnNewContact").click(()=>{
+
+    $("#contactModal").removeClass("hidden");
+
+});
+
+$("#closeModal").click(()=>{
+
+    $("#contactModal").addClass("hidden");
+
+});
+
+$("#saveContact").click(addContact);
